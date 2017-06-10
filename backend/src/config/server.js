4bc7b3bc4bac01b99a203fb9e161476e9ae6cf0d
Andre Evangelista <evangean@br.ibm.com>
2017-06-10 20:50:52 -0300

    Habilitando CORS no servidor

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,8 +3,10 @@ const port = 3003;
 //Dependecies
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
-//Start Server
+server.use(allowCors)
+    //Start Server
 server.listen(port, () => {
     console.log(`Backend is running on port ${port}`);
 })
