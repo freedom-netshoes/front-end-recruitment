d97e78e7a1b9abb02388008d6d14e88f3c056e5f
Rafael Araujo <araujrafa@gmail.com>
2017-07-25 19:56:47 -0300

    arquivos

--- public/js/app.js
+++ public/js/app.js
@@ -4,12 +4,17 @@ $(function() {
     type: "GET",
     success: function(data) {
       const obj = data.products.map(function(element) {
+        let value = element.price;
         return `<a class="c-products__card js-select" id="${element.id}">
-                  <img class="c-products__image"/>
-                  <hr class="c-products__separation" />
+                  <img src="images/image-1.jpg" class="c-products__image"/>                  
                   <h3 class="c-products__title">${element.title}</h3>
-                  <p class="c-products__current">${element.currencyFormat}<span class="c-products__price">${element.price}</span></p>
-                  <p class="c-products__installment">ou em até 3x de ${parseFloat((element.price / 3).toFixed(2))}</p>
+                  <hr class="c-products__separation" />
+                  <p class="c-products__current">
+                    ${element.currencyFormat}
+                    <span class="c-products__price">${value.toString().substring(0,3)}</span>,
+                    ${value.toString().substring(4,6)}
+                  </p>
+                  <p class="c-products__installment">ou em até ${element.installments}x de ${parseFloat((element.price / element.installments).toFixed(2))}</p>
                 </a>`;
       });
 
@@ -24,18 +29,27 @@ $(function() {
           }
         });
 
-        $('.c-cart__itens').append(`<a class="c-cart__card" id="${select[0].id}">
-                                      <img class="c-cart__image"/>
-                                      <h3 class="c-cart__title">${select[0].title}</h3>
-                                      <p class="c-cart__size-color">${select[0].availableSizes} | ${select[0].style}</h3>
-                                      <p class="c-cart__current">${select[0].currencyFormat} <span class="c-cart__price">${select[0].price}</span></p>
-                                      <p class="c-cart__installment">ou em até 3x de ${parseFloat((select[0].price / 3).toFixed(2))}</p>
-                                      <a class="c-cart__close js-close">x</a>
-                                    </a>`);
+        $('.c-cart__itens').append(`<div class="c-cart__card" id="${select[0].id}">
+                                      <img src="images/image-1.jpg" class="c-cart__image"/>
+                                      <div class="c-cart__info">
+                                        <h3 class="c-cart__title">${select[0].title}</h3>
+                                        <p class="c-cart__size-color">${select[0].availableSizes} | ${select[0].style}</p>
+                                        <p class="c-cart__qtd">Quantidade: 1</p>
+                                      </div>
+                                      <div class="c-cart__options">
+                                        <a class="c-cart__close js-close">x</a>
+                                        <p class="c-cart__current">${select[0].currencyFormat} <span class="c-cart__price">${select[0].price}</span></p>
+                                        <p class="c-cart__installment">ou em até 3x de ${parseFloat((select[0].price / 3).toFixed(2))}</p>
+                                      </div>
+                                    </div>`);
       });
 
       $('.js-close').on('click', () => {
-        $(this).remove();
+        console.log($(this));
+      });
+
+      $('.js-menu__bag').on('click', () => {
+        $('.c-cart').show();
       });
     }
   });
